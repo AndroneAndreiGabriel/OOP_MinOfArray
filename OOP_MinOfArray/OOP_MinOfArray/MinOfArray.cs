7811a179069745d5182cfea92ebbb888a5147ead
Andrei Androne <andrei.androne@outlook.com>
2022-02-12 19:56:33 +0200

    OOP_MinOfArrayRecursivity

---
+++ OOP_MinOfArray/OOP_MinOfArray/MinOfArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_MinOfArray
+{
+    public static class ArrayHelper
+    {
+        public static int MinOfArray(int[] array, int size, int index = 0)
+        {
+            if (array is null || array.Length == 0)
+            {
+                return -1;
+            }
+
+            if (index == size - 1)
+            {
+                return array[index];
+            }
+
+            int min = MinOfArray(array, size, index + 1);
+
+            if (array[index] < min)
+            {
+                return array[index];
+            }
+            else
+            {
+                return min;
+            }
+        }
+    }
+}
+
+
---
+++ OOP_MinOfArray/OOP_MinOfArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP_MinOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 9, 8, 7, 6, 1, 2, 3, 4, 5 };
+            int min = ArrayHelper.MinOfArray(array, array.Length);
+            Console.WriteLine($"Min of vector is: {min}");
+        }
+    }
+}
